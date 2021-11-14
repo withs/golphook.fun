c2f3eac65993732c7923b160ea705eab8f872e5a
Shib <contact@shib.cc>
2021-11-14 19:01:08 +0100

    Add (hello) -> Initial commit with project structure

---
+++ src/App/App.hh
@@ -0,0 +1,20 @@
+//
+// Created by fleur on 14/11/2021.
+//
+
+#ifndef GOLPHOOK_FUN_APP_HH
+#define GOLPHOOK_FUN_APP_HH
+
+#include "plog/Log.h"
+#include "nuklear.h"
+
+#include "Windows.h"
+#include "MinHook.h"
+#include "d3d9.h"
+
+class app {
+
+};
+
+
+#endif //GOLPHOOK_FUN_APP_HH
---
+++ src/main.cc
@@ -0,0 +1,9 @@
+//
+// Created by fleur on 14/11/2021.
+//
+
+#include "App/App.hh"
+
+int main() {
+    return 1;
+}
